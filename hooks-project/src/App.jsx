edd3445499d4430d9a7c8fb5c91e5a21a98ce4c0
Rehann <rehanraja3725@gmail.com>
2026-01-06 18:15:41 +0530

    HooksByRehann

---
+++ hooks-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  const Increment = () => {
+    setCounter(counter + 1)
+
+  }
+
+  return (
+    <button onClick={Increment}>Counter is : {counter}</button>
+  )
+}
+
+export default App
