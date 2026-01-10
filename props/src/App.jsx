32c97addcc9cbe8e786565b221587f56cc3ca1be
Rehann <rehanraja3725@gmail.com>
2026-01-10 17:56:49 +0530

    AllbyRehann

---
+++ props/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Hello from './Hello'
+
+function App() {
+
+
+  return (
+    <>
+      <div id='Propertese'>
+
+        <Hello name="Rehann" Description="Rehann Raja" />
+        <Hello name="Sunni" Description=" Alfaiz Makati" />
+        <Hello name="Hassu" Description="Hasnain Sunni" />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ props/src/Hello.jsx
@@ -0,0 +1,9 @@
+import './App'
+
+const Hello = ({ name, Description }) => {
+    return (
+        <div >Name :~ {name} Description :~ {Description}</div>
+    )
+}
+
+export default Hello
