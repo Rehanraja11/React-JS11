acc81b769ef8b4b0cd94969ca7e10b369af35f23
Rehann <rehanraja3725@gmail.com>
2026-02-07 16:24:10 +0530

    All By Rehann

--- Context-Practice/src/App.jsx
+++ Context-Practice/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { createContext, useContext, useState } from 'react'
+const UserData = createContext()
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [user,userHandler] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <UserData.Provider  value={{user,userHandler}}>
+    <div>
+      <h1>Hey Context APi</h1>
+      <Components1/>
+    </div>
+  </UserData.Provider>
+
+  )
+}
+export default App
+
+
+const Components1 = () => {
+  return (
+    <div>
+      <h2>Components1</h2>
+       <Components2/>
+    </div>
   )
 }
 
-export default App
+const Components2 = () => {
+  return (
+    <div>
+      <h2>Components2</h2>
+       <Components3/>
+    </div>
+  )
+}
+
+
+const Components3 = () => {
+  return (
+    <div>
+      <h2>Components3</h2>
+      <Components4/>
+    </div>
+  )
+}
+
+
+
+const Components4 = () => {
+  return (
+    <div>
+      <h2>Components4</h2>
+      <Components5/>
+    </div>
+  )
+}
+
+
+const Components5 = () => {
+ 
+  const{user,userHandler} = useContext(UserData)
+
+  return (
+    <div>
+      <input 
+      type="text"
+      value={user}
+      placeholder='Enter Your Name'
+      onChange={(e)=>userHandler(e.target.value)}
+       />
+      <h2>Components5 {user}</h2>
+    </div>
+  )
+}
+
+
+
+
+
+
